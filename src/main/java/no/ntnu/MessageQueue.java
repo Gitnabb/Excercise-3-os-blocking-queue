ad12b8424519546648c723d2f421dbb2a1f26b67
Kjetil Hammerseth <kjetilhammerseth@gmail.com>
2020-02-19 10:58:40 +0100

    init commit

---
+++ src/main/java/no/ntnu/MessageQueue.java
@@ -0,0 +1,67 @@
+package no.ntnu;
+
+
+import java.util.ArrayList;
+
+public class MessageQueue implements Channel {
+
+    private final ArrayList<Object> queue;
+    private final int size;
+
+    public MessageQueue(int size) {
+        if (size < 1) {
+            size = 1; // Do not allow weird size
+        }
+        
+        this.size = size;
+        queue = new ArrayList<>(size);
+    }
+
+    @Override
+    public synchronized void send(Object item) throws InterruptedException {
+        // TODO - block if the queue is full
+        // If the queue is full, wait for queue space.
+
+        while (this.queue.size() == this.size) {
+            wait();
+        }
+        this.queue.add(item);
+        // If an item is still in queue, notify all channels
+        if (this.queue.size() == 1) {
+            notifyAll();
+        }
+    }
+
+    // implements a nonblocking receive
+    @Override
+    public synchronized Object receive() throws InterruptedException {
+        // TODO - block if the queue is empty, and always return the first 
+        // element in the queue
+            while(queue.isEmpty()){
+                wait();
+        }
+            if(this.queue.size() == this.size){
+                notifyAll();
+            }
+            return this.queue.remove(0);
+
+    }
+
+    @Override
+    public int getNumQueuedItems() {
+        return queue.size();
+    }
+
+    /**
+     * Return comma-separated objects
+     * @return 
+     */
+    @Override
+    public String getQueueItemList() {
+        String res = "";
+        for (Object item : queue) {
+            res += item + ",";
+        }
+        return res;
+    }
+}
